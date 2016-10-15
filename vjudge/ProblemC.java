0d430c153d59ea4095d93dfdc1349464b6613644
Jess Smith <smith.jessk@gmail.com>
2016-10-15 11:41:50 -0400

    Fix some errors in vjudge/ProblemC

--- vjudge/ProblemC.java
+++ vjudge/ProblemC.java
@@ -14,31 +14,33 @@ public class ProblemC {
         // value = 1-based index
         HashMap<String, Integer> locations = new HashMap<>();
         try {
-            String[] setupParts = in.readLine().split(" ");
-            int numNumbers = Integer.parseInt(setupParts[0]);
-            int numQueries = Integer.parseInt(setupParts[1]);
-            int[] numbers = Arrays.stream(in.readLine().split(" "))
-                    .mapToInt(Integer::parseInt)
-                    .toArray();
-            for (int i = 0; i < numNumbers; i++) {
-                int n = numbers[i];
-                if (!counts.containsKey(n)) {
-                    counts.put(n, 0);
+            while (true) {
+                String[] setupParts = in.readLine().split(" ");
+                int numNumbers = Integer.parseInt(setupParts[0]);
+                int numQueries = Integer.parseInt(setupParts[1]);
+                int[] numbers = Arrays.stream(in.readLine().split(" "))
+                        .mapToInt(Integer::parseInt)
+                        .toArray();
+                for (int i = 0; i < numNumbers; i++) {
+                    int n = numbers[i];
+                    if (!counts.containsKey(n)) {
+                        counts.put(n, 0);
+                    }
+                    counts.put(n, counts.get(n) + 1);
+                    locations.put(n + "," + counts.get(n), i + 1);
                 }
-                counts.put(n, counts.get(n) + 1);
-                locations.put(n + "," + counts.get(n), i + 1);
-            }
-            StringBuilder output = new StringBuilder();
-            for (int i = 0; i < numQueries; i++) {
-                String[] parts = in.readLine().split(" ");
-                String key = parts[1] + "," + parts[0];
-                if (locations.containsKey(key)) {
-                    output.append(locations.get(key) + "\n");
-                } else {
-                    output.append("0\n");
+                StringBuilder output = new StringBuilder();
+                for (int i = 0; i < numQueries; i++) {
+                    String[] parts = in.readLine().split(" ");
+                    String key = parts[1] + "," + parts[0];
+                    if (locations.containsKey(key)) {
+                        output.append(locations.get(key) + "\n");
+                    } else {
+                        output.append("0\n");
+                    }
                 }
+                System.out.print(output);
             }
-            System.out.print(output);
         } catch (Exception e) {
 
         }
